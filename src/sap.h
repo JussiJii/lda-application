b2298ccddc3436bacd7273e8ec4ba601e65725e1
jussi-pekka <jussi.turunen@student.tut.fi>
2015-05-23 19:03:42 +0300

    Header for sap module

---
+++ src/sap.h
@@ -0,0 +1,10 @@
+//----------------------------------------------
+//
+// Helper functions for statistics and
+// probability calculations.
+//
+//----------------------------------------------
+
+void covariance(double* x, double* y, int rows, int cols);
+double avg(double* x, int rows, int cols, int index);
+double dot(double* x, double* y, int length);
